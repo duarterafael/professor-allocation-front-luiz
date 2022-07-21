a96ee53996af8b709387531a4f169c2cf95fbc3f
Luiz Oliveira <luizbarrosoliveira@gmail.com>
2022-07-21 20:50:21 -0300

    Change axios

--- src/services/axios.js
+++ src/services/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
     baseURL:
       process.env.REACT_APP_API_BASE_URL ||
-      "http://localhost:8082/",
+      "https://professor-allocation-backend.herokuapp.com/",
   });
 
   export default api;
